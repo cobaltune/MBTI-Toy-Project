b0e76a8dcfbcfc48d655583605ca55fd02e96b4f
Dorabang <psa1195@naver.com>
2023-01-28 23:43:02 +0900

    feat: Add animation

--- start/js/start.js
+++ start/js/start.js
@@ -1,11 +1,35 @@
 const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
 
-function addAnswer(answerText) {
+function addAnswer(answerText, qIdx) {
   var a = document.querySelector('.answerBox');
   var answer = document.createElement('button');
+  answer.classList.add('answerList');
+  answer.classList.add('my-3');
+  answer.classList.add('py-5');
+  answer.classList.add('mx-auto');
+  answer.classList.add('fadeIn');
   a.appendChild(answer);
   answer.innerHTML = answerText;
+
+  answer.addEventListener(
+    'click',
+    function () {
+      var children = document.querySelectorAll('.answerList');
+      for (let i = 0; i < children.length; i++) {
+        children[i].disabled = true;
+        children[i].style.WebkitAnimation = 'fadeOut 0.5s';
+        children[i].style.animation = 'fadeOut 0.5s';
+      }
+      setTimeout(() => {
+        for (let i = 0; i < children.length; i++) {
+          children[i].style.display = 'none';
+        }
+        goNext(++qIdx);
+      }, 50);
+    },
+    false,
+  );
 }
 
 function goNext(qIdx) {
@@ -13,7 +37,7 @@ function goNext(qIdx) {
   q.innerHTML = qnaList[qIdx].q;
 
   for (let i in qnaList[qIdx].a) {
-    addAnswer(qnaList[qIdx].a[i].answer);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
   }
 }
 
