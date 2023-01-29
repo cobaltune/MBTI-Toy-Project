ff397f07b255614f53b4d4703dabc5daf6b6770e
cobaltune <cobaltune@naver.com>
2023-01-29 21:57:33 +0900

    chore: Modify file path

--- start/js/share.js
+++ start/js/share.js
@@ -4,7 +4,7 @@ function sendLink() {
     content: {
       title: '십이간지 연애 유형',
       description: '십이간지로 보는 연애 유형',
-      imageUrl: './img/share.png',
+      imageUrl: '../img/share.png',
       link: {
         mobileWebUrl: 'https://twelve-test.netlify.app',
         webUrl: 'https://twelve-test.netlify.app',
