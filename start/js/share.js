722dc81f96807638899c144372d30087bd7a6151
cobaltune <cobaltune@naver.com>
2023-01-29 21:16:17 +0900

    test: Share KakaoTalk

--- start/js/share.js
+++ start/js/share.js
@@ -1,65 +1,32 @@
 function kakaoShare() {
   Kakao.Share.createDefaultButton({
-    container: '#kakaotalk-sharing-btn',
+    container: '.kakao',
     objectType: 'feed',
     content: {
-      title: '오늘의 디저트',
-      description: '아메리카노, 빵, 케익',
+      title: '딸기 치즈 케익',
+      description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
       imageUrl:
-        'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
       link: {
-        mobileWebUrl: 'https://developers.kakao.com',
-        webUrl: 'https://developers.kakao.com',
+        // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+        webUrl: 'https://twelve-test.netlify.app/',
       },
     },
-    itemContent: {
-      profileText: 'Kakao',
-      profileImageUrl:
-        'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-      titleImageUrl:
-        'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-      titleImageText: 'Cheese cake',
-      titleImageCategory: 'Cake',
-      items: [
-        {
-          item: 'Cake1',
-          itemOp: '1000원',
-        },
-        {
-          item: 'Cake2',
-          itemOp: '2000원',
-        },
-        {
-          item: 'Cake3',
-          itemOp: '3000원',
-        },
-        {
-          item: 'Cake4',
-          itemOp: '4000원',
-        },
-        {
-          item: 'Cake5',
-          itemOp: '5000원',
-        },
-      ],
-      sum: 'Total',
-      sumOp: '15000원',
-    },
     social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
+      likeCount: 286,
+      commentCount: 45,
+      sharedCount: 845,
     },
     buttons: [
       {
-        title: '웹으로 이동',
+        title: '웹으로 보기',
         link: {
           mobileWebUrl: 'https://developers.kakao.com',
           webUrl: 'https://developers.kakao.com',
         },
       },
       {
-        title: '앱으로 이동',
+        title: '앱으로 보기',
         link: {
           mobileWebUrl: 'https://developers.kakao.com',
           webUrl: 'https://developers.kakao.com',
