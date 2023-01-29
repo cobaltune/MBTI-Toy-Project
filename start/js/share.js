5d1bea81b55c46cbb6b059d8540da9fb8e598c58
cobaltune <cobaltune@naver.com>
2023-01-29 21:37:33 +0900

    test: Share KakaoTalk

--- start/js/share.js
+++ start/js/share.js
@@ -1,36 +1,28 @@
-function kakaoShare() {
-  Kakao.Share.createDefaultButton({
-    container: '.kakao',
+function sendLink() {
+  Kakao.Link.sendDefault({
     objectType: 'feed',
     content: {
-      title: '딸기 치즈 케익',
-      description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
-      imageUrl:
-        'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+      title: '십이간지 연애 유형',
+      description: '십이간지로 보는 연애 유형',
+      imageUrl: './img/share.png',
       link: {
-        // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
         mobileWebUrl: 'https://twelve-test.netlify.app',
         webUrl: 'https://twelve-test.netlify.app',
       },
     },
-    social: {
-      likeCount: 286,
-      commentCount: 45,
-      sharedCount: 845,
-    },
     buttons: [
       {
-        title: '웹으로 보기',
+        title: '웹으로 보기', //첫 번째 버튼
         link: {
-          mobileWebUrl: 'https://developers.kakao.com',
-          webUrl: 'https://developers.kakao.com',
+          mobileWebUrl: 'https://twelve-test.netlify.app', //버튼 클릭 시 이동 링크
+          webUrl: 'https://twelve-test.netlify.app',
         },
       },
       {
-        title: '앱으로 보기',
+        title: '앱으로 보기', //두 번째 버튼
         link: {
-          mobileWebUrl: 'https://developers.kakao.com',
-          webUrl: 'https://developers.kakao.com',
+          mobileWebUrl: 'https://twelve-test.netlify.app',
+          webUrl: 'https://twelve-test.netlify.app',
         },
       },
     ],
